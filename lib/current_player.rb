0b797fd046f7a8be637a61420714ed0624f66386
Celal Cankaya <celalcankaya@outlook.com>
2019-07-09 20:21:41 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,12 @@
+def turn_count(board)
+  round = 0
+  board.each do |item|
+    if item "X" or "O"
+      round +=1
+  return round
+  end
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
