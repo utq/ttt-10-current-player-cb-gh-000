b9a6bc31cb436f027816d2df5ad6231b91cbaddc
Celal Cankaya <celalcankaya@outlook.com>
2019-07-09 20:29:14 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,13 @@
+def turn_count(board)
+  round = 0
+  board.each do |boarditem|
+    if boarditem  == "X" or boarditem  == "O"
+      round +=1
+    end
+  end
+  return round
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
